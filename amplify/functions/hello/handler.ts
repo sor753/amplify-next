bc33dc541197d9330fb74ef2e8be665c7bb4f13f
show kawazoe <show753@icloud.com>
2025-06-25 05:38:44 +0900

    add simple lambda function

---
+++ amplify/functions/hello/handler.ts
@@ -0,0 +1,7 @@
+import { Schema } from '../../data/resource';
+type handlerType = Schema['sayHello']['functionHandler'];
+
+export const handler: handlerType = async (event, context) => {
+  const { name } = event.arguments;
+  return `Hello, ${name}!`;
+};
